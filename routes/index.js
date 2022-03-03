04ddc5baa26b892dabd9e6dc67a73654a2e176a9
Ripunjoy <ripunjoysarma2000@gmail.com>
2022-03-03 09:39:31 +0530

    some silly mistakes are corrected

--- routes/index.js
+++ routes/index.js
@@ -1,214 +1,228 @@
-const express=require('express');
+const express = require("express");
 const router = express.Router();
-const bodyParser = require('body-parser');
-const bcrypt = require('bcrypt');
-const hospital = require('../models/hospital.js');
-const Doctor = require('../models/doctor.js');
-const passport = require('passport');
-const session = require('express-session');
-const cookieParser = require('cookie-parser');
-const flash = require('connect-flash');
-const mongoose=require("mongoose")
-
-
+const bodyParser = require("body-parser");
+const bcrypt = require("bcrypt");
+const hospital = require("../models/hospital.js");
+const Doctor = require("../models/doctor.js");
+const passport = require("passport");
+const session = require("express-session");
+const cookieParser = require("cookie-parser");
+const flash = require("connect-flash");
+const mongoose = require("mongoose");
 
 const { findById, db } = require("../models/hospital.js");
 
-router.use(cookieParser('secret'));
-router.use(session({
-    secret: 'secret',
+router.use(cookieParser("secret"));
+router.use(
+  session({
+    secret: "secret",
     maxAge: 3600000,
     resave: true,
     saveUninitialized: true,
-
-}))
+  })
+);
 router.use(passport.initialize());
 router.use(passport.session());
 
 router.use(flash());
-router.use(function(req,res,next){
-    res.locals.success_message = req.flash('success_message');
-    res.locals.error_message = req.flash('error_message');
-    res.locals.error = req.flash('error');
-    next();
+router.use(function (req, res, next) {
+  res.locals.success_message = req.flash("success_message");
+  res.locals.error_message = req.flash("error_message");
+  res.locals.error = req.flash("error");
+  next();
 });
 
-const ensureAuthenticated = function(req,res,next){
-    if(req.isAuthenticated()){
-        res.set('Cache-Control','no-cache,private,no-store,must-revalidate,post-check=0,pre-check=0');
-        return next();
-    }else res.redirect('/hospitalLogin');
-}
+const ensureAuthenticated = function (req, res, next) {
+  if (req.isAuthenticated()) {
+    res.set(
+      "Cache-Control",
+      "no-cache,private,no-store,must-revalidate,post-check=0,pre-check=0"
+    );
+    return next();
+  } else res.redirect("/hospitalLogin");
+};
 
-router.use(bodyParser.urlencoded({extended :true}));
+router.use(bodyParser.urlencoded({ extended: true }));
 router.use(bodyParser.json());
 
 router.use(express.json());
 router.use(express.urlencoded());
 
-router.get('/',(req,res)=>{
-    res.render('hospitalRegister');
-})
-
-router.post('/hospitalRegister',(req,res)=>{
-    let { name, email, location, password, confirmPassword }= req.body;
-    if(!name || !email || !password || !confirmPassword || !location) {
-      err="Please fill all the fields";
-      res.render('hospitalRegister', { 'err' : err });
-    }
-    if(password != confirmPassword){
-        err='Passwords dont match';
-        res.render('register',{ 'err':err, 'email':email, 'name':name, 'password':password, 'location':location});
-    }
-    if(typeof err == 'undefined'){
-      hospital.findOne({email:email}, function(err,data){
-          if(err) throw err;
-          if(data){
-              err='Hospital already registered';
-              res.render('register', {'name':name, 'err': err});
-          }
-          else{
-              bcrypt.genSalt(10, (err,salt)=>{
-                  if(err) throw err;
-                  bcrypt.hash(password, salt, (err,hash)=>{
-                      if(err) throw err;
-                      password = hash;
-                      hospital({
-                          name, email, password, location
-                      }).save((err,data)=>{
-                          if(err) throw err;
-                          req.flash('success_message', "Please Login to Continue");
-                          res.redirect('/login');
-                      });
-                  })
-              })
-          }
-      })
-    }
-    
+router.get("/", (req, res) => {
+  res.render("hospitalRegister");
 });
 
-var localStrategy = require('passport-local').Strategy;
-passport.use('hospital',new localStrategy({usernameField:'email'}, (email,password,done)=>{
-    hospital.findOne({ email:email},(err,data)=>{
-        if(err) throw err;
-        if(!data){
-            return done(null,false, { message: 'Hospital Not Registered'});
-        }
-        bcrypt.compare(password, data.password, (err,match)=>{
-            if(err) return done(null,false);
-            if(!match) return done(null,false, { message: 'Password is Incorrect'});
-            if(match) return done(null,data);
-        })
-    })
-}));
-
-passport.serializeUser(function(user,cb){
-    cb(null,user.id);
-});
-
-passport.deserializeUser(function(id,cb){
-    hospital.findById(id, function(err,user){
-        if(err) cb(err);
-        if(user) cb(null,user);
-        // else{
-        //     admin.findById(id, function(err,user){
-        //         if(err) cb(err);
-        //         cb(null,user);
-        //     })
-        // }
-    })
-});
-
-router.get('/hospitallogin', (req,res)=>{
-    res.render('hospitalLogin');
-});
-
-router.post('/hospitalLogin', (req,res,next)=>{
-    passport.authenticate('hospital',{
-        failureRedirect: '/hospitalLogin',
-        successRedirect: '/hospitalDashboard',
-        failureFlash: true,
-    })(req,res,next);
-});
-
-router.get('/logout', (req,res)=>{
-    req.logout();
-    res.redirect('/hospitalLogin');
-});
-
-router.get('/hospitalDashboard', ensureAuthenticated, (req,res)=>{
-    // doctors.find({}.populate, (err,data)=>{
-    //     res.render('hospitalDashboard', {doctor: data});
-    // })
-    hospital.findOne({_id:req.user._id}).populate({path:"doctors",model:Doctor}).exec(function(err,data){
-        if(err) throw err;
-        else{
-            console.log(data.doctors);
-            res.render('hospitalDashboard', {user:data});
-        }
-    })
-
-    // hospital.findOne({_id:req.user._id}).populate({path:"doctors",model:Doctor}).then(user => {
-    //     res.json(user); 
-    //  });
-
-    
-    // let hospital_id = req.user._id;
-    // res.render('hospitalDashboard', {user:req.user});
-    // console.log(req.user);
-})
-
-router.get('/get', ensureAuthenticated, (req,res)=>{
-    hospital.findOne({_id:req.user._id}).populate({path:"doctors",model:Doctor}).exec(function(err,data){
-        if(err) throw err;
-        else{
-            console.log(data.doctors);
-            res.send(data);
-        }
-    })
-})
-
-router.get('/doctor/add/:id', ensureAuthenticated, (req,res)=>{
-    res.render('addDoctor', {user: req.user});
-})
-
-router.post('/add/:id', ensureAuthenticated, async(req,res)=>{
-    
-    const reg = req.body.registration;
-    const name = req.body.name;
-    const spec = req.body.specialization;
-    console.log(req.body)
-
-    hospital.findById(req.params.id,(err,hospital)=>{
-        if(err) throw err;
-        else{
-            Doctor.create({registration: reg,name:name,specialization:spec}, (err,data)=>{
-                if(err) throw err;
-                else{
-                    console.log(data);
-                    hospital.doctors.push(data);
-                    hospital.save(); 
-                }
-                // if(data){
-                //     err ='Doctor already registered';
-                //     res.render('addDoctor');
-                // }
-                
+router.post("/hospitalRegister", (req, res) => {
+  let { name, email, location, password, confirmPassword } = req.body;
+  if (!name || !email || !password || !confirmPassword || !location) {
+    err = "Please fill all the fields";
+    res.render("hospitalRegister", { err: err });
+  }
+  if (password != confirmPassword) {
+    err = "Passwords dont match";
+    res.render("hospitalRegister", {
+      err: err,
+      email: email,
+      name: name,
+      password: password,
+      location: location,
+    });
+  }
+  if (typeof err == "undefined") {
+    hospital.findOne({ email: email }, function (err, data) {
+      if (err) throw err;
+      if (data) {
+        err = "Hospital already registered";
+        res.render("hospitalRegister", { name: name, err: err });
+      } else {
+        bcrypt.genSalt(10, (err, salt) => {
+          if (err) throw err;
+          bcrypt.hash(password, salt, (err, hash) => {
+            if (err) throw err;
+            password = hash;
+            hospital({
+              name,
+              email,
+              password,
+              location,
+            }).save((err, data) => {
+              if (err) throw err;
+              req.flash("success_message", "Please Login to Continue");
+              res.redirect("/hospitallogin");
             });
-            
+          });
+        });
+      }
+    });
+  }
+});
+
+var localStrategy = require("passport-local").Strategy;
+passport.use(
+  "hospital",
+  new localStrategy({ usernameField: "email" }, (email, password, done) => {
+    hospital.findOne({ email: email }, (err, data) => {
+      if (err) throw err;
+      if (!data) {
+        return done(null, false, { message: "Hospital Not Registered" });
+      }
+      bcrypt.compare(password, data.password, (err, match) => {
+        if (err) return done(null, false);
+        if (!match)
+          return done(null, false, { message: "Password is Incorrect" });
+        if (match) return done(null, data);
+      });
+    });
+  })
+);
+
+passport.serializeUser(function (user, cb) {
+  cb(null, user.id);
+});
+
+passport.deserializeUser(function (id, cb) {
+  hospital.findById(id, function (err, user) {
+    if (err) cb(err);
+    if (user) cb(null, user);
+    // else{
+    //     admin.findById(id, function(err,user){
+    //         if(err) cb(err);
+    //         cb(null,user);
+    //     })
+    // }
+  });
+});
+
+router.get("/hospitallogin", (req, res) => {
+  res.render("hospitalLogin");
+});
+
+router.post("/hospitalLogin", (req, res, next) => {
+  passport.authenticate("hospital", {
+    failureRedirect: "/hospitalLogin",
+    successRedirect: "/hospitalDashboard",
+    failureFlash: true,
+  })(req, res, next);
+});
+
+router.get("/logout", (req, res) => {
+  req.logout();
+  res.redirect("/hospitalLogin");
+});
+
+router.get("/hospitalDashboard", ensureAuthenticated, (req, res) => {
+  // doctors.find({}.populate, (err,data)=>{
+  //     res.render('hospitalDashboard', {doctor: data});
+  // })
+  hospital
+    .findOne({ _id: req.user._id })
+    .populate({ path: "doctors", model: Doctor })
+    .exec(function (err, data) {
+      if (err) throw err;
+      else {
+        console.log(data.doctors);
+        res.render("hospitalDashboard", { user: data });
+      }
+    });
+
+  // hospital.findOne({_id:req.user._id}).populate({path:"doctors",model:Doctor}).then(user => {
+  //     res.json(user);
+  //  });
+
+  // let hospital_id = req.user._id;
+  // res.render('hospitalDashboard', {user:req.user});
+  // console.log(req.user);
+});
+
+router.get("/get", ensureAuthenticated, (req, res) => {
+  hospital
+    .findOne({ _id: req.user._id })
+    .populate({ path: "doctors", model: Doctor })
+    .exec(function (err, data) {
+      if (err) throw err;
+      else {
+        console.log(data.doctors);
+        res.send(data);
+      }
+    });
+});
+
+router.get("/doctor/add/:id", ensureAuthenticated, (req, res) => {
+  res.render("addDoctor", { user: req.user });
+});
+
+router.post("/add/:id", ensureAuthenticated, async (req, res) => {
+  const reg = req.body.registration;
+  const name = req.body.name;
+  const spec = req.body.specialization;
+  console.log(req.body);
+
+  hospital.findById(req.params.id, (err, hospital) => {
+    if (err) throw err;
+    else {
+      Doctor.create(
+        { registration: reg, name: name, specialization: spec },
+        (err, data) => {
+          if (err) throw err;
+          else {
+            console.log(data);
+            hospital.doctors.push(data);
+            hospital.save();
+          }
+          // if(data){
+          //     err ='Doctor already registered';
+          //     res.render('addDoctor');
+          // }
         }
-    })
+      );
+    }
+  });
 
+  // hos.doctors.push(doc._id);
+  res.redirect("/hospitalDashboard");
+});
 
-    
-
-    
-    // hos.doctors.push(doc._id);
-    res.redirect('/hospitalDashboard');
-})
-
-router.post("/a",function(req,res){
-    res.send(req.body);
-})
-module.exports=router;
+router.post("/a", function (req, res) {
+  res.send(req.body);
+});
+module.exports = router;
